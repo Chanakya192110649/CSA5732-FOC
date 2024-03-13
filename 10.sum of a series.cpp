77c36959e0ea06aebf2b2e954dc71cdc29c4d823
Chanakya192110649 <112469749+Chanakya192110649@users.noreply.github.com>
2024-03-13 13:48:42 +0530

    Update 10.sum of a series.cpp

--- 10.sum of a series.cpp
+++ 10.sum of a series.cpp
@@ -1,13 +1,20 @@
-#include<stdio.h>
-int main(){
-	int i,num,a=1;
-	
-	printf("ENTER THE integer :");
-	scanf("%d",&num);
-	
-	for(i=1;i<=num;i++){
-		a +=i;
-	
-	}
-		printf(" %d",a);
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0, i, j;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 0; i <= n; i++) {
+        
+        for (j = 1; j <= i; j++) {
+            
+            sum += j;
+        }
+    }
+
+    printf("Sum of the series is: %d\n", sum);
+
+    return 0;
 }
